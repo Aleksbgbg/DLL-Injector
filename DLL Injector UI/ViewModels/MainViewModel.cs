23c084332909dcdef32ebae29ad8f11e1f5118db
Aleksbgbg <kozichka01@abv.bg>
2018-12-05 15:22:23 +0000

    [MainViewModel] Provide a list of all processes and allow the user to choose, via ComboBox.

--- DLL Injector UI/ViewModels/MainViewModel.cs
+++ DLL Injector UI/ViewModels/MainViewModel.cs
@@ -1,8 +1,37 @@
 ﻿namespace Dll.Injector.ViewModels
 {
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Windows.Data;
+
+    using Caliburn.Micro;
+
+    using Dll.Injector.Services.Interfaces;
     using Dll.Injector.ViewModels.Interfaces;
 
     internal class MainViewModel : ViewModelBase, IMainViewModel
     {
+        public MainViewModel(IProcessService processService)
+        {
+            Processes = new BindableCollection<Process>(processService.ListProcesses());
+
+            CollectionViewSource.GetDefaultView(Processes).SortDescriptions.Add(new SortDescription(nameof(Process.StartTime), ListSortDirection.Descending));
+        }
+
+        public IObservableCollection<Process> Processes { get; }
+
+        private Process _selectedProcess;
+        public Process SelectedProcess
+        {
+            get => _selectedProcess;
+
+            set
+            {
+                if (_selectedProcess == value) return;
+
+                _selectedProcess = value;
+                NotifyOfPropertyChange(() => SelectedProcess);
+            }
+        }
     }
 }
